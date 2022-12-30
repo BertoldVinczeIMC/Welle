5a9810ded1800b3d8871e0f75fdd113b87eec786
BertoldVinczeIMC <21imc10072@fh-krems.ac-at>
2022-12-30 18:52:19 +0100

    Added ref

--- frontend/src/components/Reference/reference.js
+++ frontend/src/components/Reference/reference.js
@@ -5,9 +5,10 @@ import { motion, useAnimation } from "framer-motion";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Carousel from "react-bootstrap/Carousel";
+import Image from "react-bootstrap/Image";
 
 import ref1 from "../../assets/img/ref1.jpg";
+import ref2 from "../../assets/img/ref2.png";
 
 export default function Reference() {
   const elementRef = useRef(null);
@@ -42,38 +43,161 @@ export default function Reference() {
       >
         <Container fluid>
           <Row className="justify-content-center">
-            <Col lg={12} md={12} sm={12} xs={12} className="service-card">
-              <h2 className="text-center section-title">
-                What to expect from us? Take a look in this
-              </h2>
-              <Container>
-                <Carousel>
-                  <Carousel.Item>
-                    <img
-                      className="d-block w-100"
-                      src={ref1}
-                      alt="First slide"
-                      style={{ maxWidth: "100%", maxHeight: "600px" }}
-                    />
-                  </Carousel.Item>
-                  <Carousel.Item>
-                    <img
-                      className="d-block w-100"
-                      src={ref1}
-                      alt="Second slide"
-                      style={{ maxWidth: "100%", maxHeight: "600px" }}
-                    />
-                  </Carousel.Item>
-                  <Carousel.Item>
-                    <img
-                      className="d-block w-100"
-                      src={ref1}
-                      alt="Third slide"
-                      style={{ maxWidth: "100%", maxHeight: "600px" }}
-                    />
-                  </Carousel.Item>
-                </Carousel>
-              </Container>
+            <Col
+              lg={12}
+              md={12}
+              sm={12}
+              xs={12}
+              className="text-center service-card"
+            >
+              <h1 className="section-title">References</h1>
+              <p className="service-description">
+                Take a look at some of our projects and see what we can do for
+                you. We are always happy to help. We strive to provide the best
+                service possible.
+              </p>
+            </Col>
+          </Row>
+          <Row className="justify-content-center">
+            <Col lg={3} md={6} sm={12} className="ref service-card scard" onClick={()=>{
+              // open new tab
+              window.open("https://alis-kebap-c3a40b.ingress-comporellon.ewp.live")
+            }}>
+              <motion.div
+                ref={elementRef}
+                animate={animationControls}
+                initial={{ opacity: 0 }}
+                variants={{
+                  visible: {
+                    x: 0,
+                    opacity: 1,
+                    transition: {
+                      duration: 2,
+                    },
+                  },
+                }}
+              >
+                <Image fluid src={ref2} className="scard-img" />
+                {/* image row */}
+                <Row className="justify-content-center text-center service-title">
+                  Haus des Istanbul
+                </Row>
+                {/* text row */}
+                <Row className="justify-content-center">
+                  <Col className="text-center">
+                    <p className="service-description">
+                      Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry. Lorem Ipsum has been the industry's
+                      standar text.
+                    </p>
+                  </Col>
+                </Row>
+              </motion.div>
+            </Col>
+            <Col lg={3} md={6} sm={12} className="ref service-card scard" onClick={()=>{
+              // open new tab
+              window.open("https://alis-kebap-c3a40b.ingress-comporellon.ewp.live")
+            }}>
+              <motion.div
+                ref={elementRef}
+                animate={animationControls}
+                initial={{ opacity: 0 }}
+                variants={{
+                  visible: {
+                    x: 0,
+                    opacity: 1,
+                    transition: {
+                      duration: 2,
+                    },
+                  },
+                }}
+              >
+                <Image fluid src={ref2} className="scard-img" />
+                {/* image row */}
+                <Row className="justify-content-center text-center service-title">
+                  Haus des Istanbul
+                </Row>
+                {/* text row */}
+                <Row className="justify-content-center">
+                  <Col className="text-center">
+                    <p className="service-description">
+                      Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry. Lorem Ipsum has been the industry's
+                      standar text.
+                    </p>
+                  </Col>
+                </Row>
+              </motion.div>
+            </Col>
+            <Col lg={3} md={6} sm={12} className="ref service-card scard" onClick={()=>{
+              // open new tab
+              window.open("https://alis-kebap-c3a40b.ingress-comporellon.ewp.live")
+            }}>
+              <motion.div
+                ref={elementRef}
+                animate={animationControls}
+                initial={{ opacity: 0 }}
+                variants={{
+                  visible: {
+                    x: 0,
+                    opacity: 1,
+                    transition: {
+                      duration: 2,
+                    },
+                  },
+                }}
+              >
+                <Image fluid src={ref2} className="scard-img" />
+                {/* image row */}
+                <Row className="justify-content-center text-center service-title">
+                  Haus des Istanbul
+                </Row>
+                {/* text row */}
+                <Row className="justify-content-center">
+                  <Col className="text-center">
+                    <p className="service-description">
+                      Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry. Lorem Ipsum has been the industry's
+                      standar text.
+                    </p>
+                  </Col>
+                </Row>
+              </motion.div>
+            </Col>
+            <Col lg={3} md={6} sm={12} className="ref service-card scard" onClick={()=>{
+              // open new tab
+              window.open("https://alis-kebap-c3a40b.ingress-comporellon.ewp.live")
+            }}>
+              <motion.div
+                ref={elementRef}
+                animate={animationControls}
+                initial={{ opacity: 0 }}
+                variants={{
+                  visible: {
+                    x: 0,
+                    opacity: 1,
+                    transition: {
+                      duration: 2,
+                    },
+                  },
+                }}
+              >
+                <Image fluid src={ref2} className="scard-img" />
+                {/* image row */}
+                <Row className="justify-content-center text-center service-title">
+                  Haus des Istanbul
+                </Row>
+                {/* text row */}
+                <Row className="justify-content-center">
+                  <Col className="text-center">
+                    <p className="service-description">
+                      Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry. Lorem Ipsum has been the industry's
+                      standar text.
+                    </p>
+                  </Col>
+                </Row>
+              </motion.div>
             </Col>
           </Row>
         </Container>
